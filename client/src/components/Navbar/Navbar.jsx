ca8c922aee2032ce8d465fbbb510d9ec64b80718
HamiAvet <hamov2003@gmail.com>
2025-04-23 21:15:06 +0200

    d

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { CartContext } from "../../context/CartContext";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-scroll";
 import "./Navbar.scss";
+
+
 
 const Navbar = () => {
   const { logout, isLogin } = useContext(AuthContext);
@@ -11,6 +13,36 @@ const Navbar = () => {
   
   const location = useLocation();
   const currentPath = location.pathname;
+
+  const DropDown = () => {
+    const [isOpen, setIsOpen] = useState(false)
+  
+    return (
+      <div>
+        <button onClick={() => setIsOpen(!isOpen)}>aaaaaaa</button>
+        {isOpen && (
+          <ul
+            style={{
+              position: "absolute",
+              right: 0,
+              top: "110%",
+              background: "white",
+              boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
+              borderRadius: "4px",
+              listStyle: "none",
+              padding: "8px 0",
+              margin: 0,
+              minWidth: "160px",
+              zIndex: 1000,
+            }}
+          >
+            <li><a className="button" href="/login" onClick={logout}>Logout</a></li>
+            <li><a className="button" href="/myaccount">MyAccount</a></li>
+          </ul>
+        )}
+      </div>
+    )
+  }
 
   useEffect(() => {
     updateCartCount();
@@ -49,9 +81,7 @@ const Navbar = () => {
                 <img src="/images/shopping_cart.png" alt="cart" />
                 {cartCount > 0 && <p className="count">{cartCount}</p>}
               </a>
-              <a className="button" href="/login" onClick={logout}>
-                Logout
-              </a>
+              <DropDown/>
             </>
           ) : (
             <>
